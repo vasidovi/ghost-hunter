ae5485a6ee002b5ee1997a43d2226457af25ad5f
Vasidovi <vasido@gmail.com>
2019-03-02 19:30:53 +0200

    changed port settings

--- src/ws/main.js
+++ src/ws/main.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 app.use("/", express.static('src/frontend'));
 app.use('/images', express.static('res/images'));
